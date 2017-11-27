a6895bf71478eb6e1d7e70d3c004c8d2230f6f44
brandonj1388 <32871687+brandonj1388@users.noreply.github.com>
2017-11-27 01:05:48 -0500

    To go with FInal Project commit
    The changes made to the Offensive, Defensive, and Manager classes

--- Manager.java
+++ Manager.java
@@ -3,41 +3,46 @@ package draftProject;
 
 import java.util.ArrayList;
 
-public class Manager {
-	ArrayList<NFL_Player> roster;
+public class Manager extends NFL_Player {
+	public static ArrayList<NFL_Player> roster;
 
 	// manage player class
 	public Manager() {
 		roster = new ArrayList<NFL_Player>();
-		createPlayers();
+		createOffensivePlayers();
+		createDefensivePlayers();
 	}
 
 	// create players & add to roster
-	public void createPlayers() {
+	public void createOffensivePlayers() {
 		// player 1
-		NFL_Player one = new NFL_Player("Shawn Jones", "Quarterback", "Ohio State", 13, 18, "6 foot, 10 inches",
-				"205 lbs", 5025, 36, 0, 0, 45, 40);
+		OffensivePlayer one = new OffensivePlayer("Shawn Jones", "Quarterback", "Ohio State", 18, 13,
+				"6 foot, 10 inches", "205 lbs", 5000, 25, 45, 40);
 		roster.add(one);
+
 		// player 2
-		one = new NFL_Player("Patrick Muns", "Running Back", "Michigan", 33, 19, "5 foot, 8 inches", "230 lbs", 2384,
-				18, 0, 0, 36, 36);
+		one = new OffensivePlayer("Patrick Muns", "Running Back", "Michigan", 19, 33, "5 foot, 8 inches", "230 lbs",
+				2550, 13, 36, 36);
 		roster.add(one);
 		// player 3
-		one = new NFL_Player("Jason Bog", "Wide Reciever", "LSU", 88, 18, "6 foot, 11 inches", "196 lbs", 3000, 25, 0,
-				0, 19, 10);
+		one = new OffensivePlayer("Jason Bog", "Wide Reciever", "LSU", 18, 88, "6 foot, 11 inches", "196 lbs", 1830, 18,
+				19, 10);
 		roster.add(one);
+	}
+
+	public void createDefensivePlayers() {
 		// player 4
-		one = new NFL_Player("Marcus Lawry", "Conerback", "Alabama", 56, 19, "6 foot, 2 inches", "220 lbs", 0, 0, 6,
-				120, 32, 20);
-		roster.add(one);
+		DefensivePlayer two = new DefensivePlayer("Marcus Lawry", "Conerback", "Alabama", 19, 56, "6 foot, 2 inches",
+				"220 lbs", 5, 115, 32, 20);
+		roster.add(two);
 		// player 5
-		one = new NFL_Player("Brandon Stevens", "Linebacker", "Clemson", 48, 20, "5 foot, 9 inches", "215 lbs", 0, 0,
-				20, 200, 29, 10);
-		roster.add(one);
+		two = new DefensivePlayer("Brandon Stevens", "Linebacker", "Clemson", 20, 48, "5 foot, 9 inches", "215 lbs", 15,
+				200, 29, 10);
+		roster.add(two);
 		// player 6
-		one = new NFL_Player("Brian Adams", "Safety", "Penn State", 25, 20, "6 foot, 0 inches", "196 lbs", 0, 0, 3, 125,
+		two = new DefensivePlayer("Brian Adams", "Safety", "Penn State", 20, 25, "6 foot, 0 inches", "196 lbs", 2, 150,
 				20, 0);
-		roster.add(one);
+		roster.add(two);
 	}
 
 	public String toString() {
---
+++ OffensivePlayer.java
@@ -0,0 +1,76 @@
+//This is my own work, Brandon Johnson
+package draftProject;
+
+public class OffensivePlayer extends NFL_Player {
+	private int yards;
+	private int touchdowns;
+
+	public OffensivePlayer(String name, String position, String college, int age, int jerseyNum, String height,
+			String weight, int yards, int touchdowns, int gamesPlayed, int gamesStarted) {
+		super(name, position, college, age, jerseyNum, height, weight, gamesPlayed, gamesStarted);
+		setName(name);
+		setPosition(position);
+		setCollege(college);
+		setAge(age);
+		setJerseyNum(jerseyNum);
+		setHeight(height);
+		setWeight(weight);
+		this.yards = yards;
+		this.touchdowns = touchdowns;
+		setGamesPlayed(gamesPlayed);
+		setGamesStarted(gamesStarted);
+	}
+
+	public OffensivePlayer(int yards, int touchdowns) {
+		this.yards = yards;
+		this.touchdowns = touchdowns;
+
+	}
+
+	public OffensivePlayer() {
+		yards = 0;
+		touchdowns = 0;
+	}
+
+	/**
+	 * @return the yards
+	 */
+	public int getYards() {
+		return yards;
+	}
+
+	/**
+	 * @param yards
+	 *            the yards to set
+	 */
+	public void setYards(int yards) {
+		this.yards = yards;
+	}
+
+	/**
+	 * @return the touchdowns
+	 */
+	public int getTouchdowns() {
+		return touchdowns;
+	}
+
+	/**
+	 * @param touchdowns
+	 *            the touchdowns to set
+	 */
+	public void setTouchdowns(int touchdowns) {
+		this.touchdowns = touchdowns;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "\n" + "OffensivePlayer" + super.toString() + "yards: " + yards + "\n" + "touchdowns: " + touchdowns
+				+ "\n";
+	}
+
+}
